817eb49d3941dc7b2497b5bb526a693bf2265ddd
mudrakpatel <mudrakpatel96@gmail.com>
2017-02-25 15:16:22 -0500

    Ex2 task 1 complete

--- MudrakPatel_Lab04_02/Program.cs
+++ MudrakPatel_Lab04_02/Program.cs
@@ -10,6 +10,32 @@ namespace MudrakPatel_Lab04_02
     {
         static void Main(string[] args)
         {
+            //Declare a LinkedList of Student objects and add initial students in it
+            LinkedList<Student> studentLinkedList = new LinkedList<Student>();
+            studentLinkedList.AddLast(new Student("Wade",0));
+            studentLinkedList.AddLast(new Student("Peter", 1));
+            studentLinkedList.AddLast(new Student("Stephen", 2));
+            studentLinkedList.AddLast(new Student("Goku", 3));
+            studentLinkedList.AddLast(new Student("Gon", 4));
+            studentLinkedList.AddLast(new Student("Happy", 5));
+            //Print all current students
+            Console.WriteLine("\n>>>List of initially enrolled students...\n");
+            foreach (var student in studentLinkedList)
+            {
+                Console.WriteLine("Name: {0,3}", student.name + " ID: " + student.id);
+            }
+            Console.WriteLine();
+            //Add a neww Student object by calling AddLinkedListItem() method
+            AddLinkedListItem(studentLinkedList, new Student("Lucy", 6));
+        }
+
+        public static void AddLinkedListItem(LinkedList<Student> inputLinkedList, Student studentObject)
+        {
+            Console.WriteLine("Adding a student to the LinkedList...\n");
+            LinkedListNode<Student> studentNode = new LinkedListNode<Student>(studentObject);
+            inputLinkedList.AddLast(studentNode);
+            Console.WriteLine("\nNew Student object added...\nName: " 
+                              + studentObject.name + " ID: " + studentObject.id);
         }
     }
 }
---
+++ MudrakPatel_Lab04_02/Student.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MudrakPatel_Lab04_02
+{
+    class Student
+    {
+        //Properties
+        public int id { get; set; }
+        public string name { get; set; }
+
+        //Methods and constructors
+        //Constructor
+        public Student(string Name, int ID)
+        {
+            name = Name;
+            id = ID;
+        }
+
+        //Overridden ToString() method
+        public override string ToString()
+        {
+            return String.Format("\nName: {0,3}; ID: {1,6}\n", name, id);
+        }
+    }
+}
